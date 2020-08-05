10a7b6d760f3fdaa1fc4fc149c7703bd38787521
Kenneth Ofosu <kofosu89@gmail.com>
2020-08-05 15:37:17 -0400

    Add layout links and DRY code Add links to connect pages of app. Create partials to clean up controller and views code.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
+    
   end
 
   def index
@@ -13,33 +14,44 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was created successfully."
-      redirect_to @article
+      flash_status_messages
     else
       render 'new'
     end
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :descriptiom))
-      flash[:notice] = 'Article was updated successfully.'
-      redirect_to @article
+    if @article.update(article_params)
+      flash_status_messages
     else
       render 'edit'
     end
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+    def flash_status_messages
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    end
 
 end
