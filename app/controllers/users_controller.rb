f396a46097b0373abf1815cca95749d855c1946d
Kenneth Ofosu <kofosu89@gmail.com>
2020-08-07 19:53:48 -0400

    Create authentication helper methods; refactor users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:show, :edit, :update]
   def new
     @user = User.new
   end
@@ -9,13 +9,13 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Omega Blog, #{@user.username}. You have successfully signed up."
       redirect_to articles_path
     else
@@ -24,11 +24,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       flash_status_messages
     else
@@ -45,5 +45,9 @@ class UsersController < ApplicationController
   def flash_status_messages
     flash[:notice] = "Your account was successfully updated"
     redirect_to @user
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
